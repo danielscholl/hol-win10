fab269aa7ab1dd13ff90489b1402afa55a93439b
Daniel Scholl <dascholl@microsoft.com>
2020-02-12 14:26:12 -0600

    Added postman

--- setup.ps1
+++ setup.ps1
@@ -5,13 +5,15 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Assign Packages to Install
-$Packages = 'visualstudiocode', 'docker-desktop'
+$Packages = 'visualstudiocode', 'docker-desktop', 'postman'
+
+# choco install wsl-debiangnulinux
 
 #Install Packages
 ForEach ($PackageName in $Packages)
 {choco install $PackageName -y}
 
-Add-LocalGroupMember -Group "docker-users" -Member "azureUser"
+Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
 
 #Reboot
 Restart-Computer
