3678001ed02910a90a12fa7dacb4df5e791de8f1
Daniel Scholl <dascholl@microsoft.com>
2020-02-12 15:15:45 -0600

    Ubuntu

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 Add-LocalGroupMember -Group "docker-users" -Member $env:UserName
 
 #Assign Packages to Install
-$Packages = 'postman', 'wsl-debiangnulinux'
+$Packages = 'postman', 'wsl-ubuntu-1804'
 
 #Install Packages
 ForEach ($PackageName in $Packages)
