da83f1a6f2713243d95353eb370d3c82b34a510f
Daniel Scholl <dascholl@microsoft.com>
2020-04-04 18:01:24 -0500

    Added software to install

--- setup.ps1
+++ setup.ps1
@@ -1,16 +1,44 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
 #Install Chocolatey
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-#Assign Packages to Install
-$Packages = 'visualstudiocode', 'docker-desktop'
+#Assign Chocolatey Packages to Install
+$Packages = `
+    'git', `
+    'git-fork', `
+    'microsoft-edge', `
+    'visualstudiocode', `
+    'docker-desktop'
+
 
 #Install Packages
 ForEach ($PackageName in $Packages)
 {choco install $PackageName -y}
 
+#Install Azure PowerShell
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-ExecutionPolicy Bypass -Scope Process -Force; Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
+
+#Install Azure CLI for Windows
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+
+#Enable WSL
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+
+#Download and Install Ubuntu
+Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
+Add-AppxPackage -Path ~/Ubuntu.appx
+
 Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
+
+#Bring down Desktop Shortcuts
+$zipDownload = "https://github.com/danielscholl/hol-win10/blob/master/shortcuts.zip?raw=true"
+$downloadedFile = "D:\shortcuts.zip"
+$vmFolder = "C:\Users\Public\Desktop"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest $zipDownload -OutFile $downloadedFile
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::ExtractToDirectory($downloadedFile, $vmFolder)
 
 #Reboot
 Restart-Computer
